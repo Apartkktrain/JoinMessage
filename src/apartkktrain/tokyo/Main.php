5e827887ba47971dbbb1d1edb39ff8cb67b0e515
Apartkktrain <64818647+Apartkktrain@users.noreply.github.com>
2020-05-23 11:37:12 +0900

    クラス外に書いてあるのを訂正。

--- src/apartkktrain/tokyo/Main.php
+++ src/apartkktrain/tokyo/Main.php
@@ -6,7 +6,12 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-
+use pocketmine\Server;
+use pocketmine\block\Block;
+use pocketmine\utils\Config;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
 
 
 
@@ -14,15 +19,20 @@ class Main extends pluginBase implements Listener
 {
     public function onEnable()
     {
-        ///$this->getLogger()->notice("tokyover.0.0.1の読み込み成功");
-        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        $this->getLogger()->notice("tokyover.0.0.1の読み込み成功");
+        $this->getLogger()->notice("システム構築に成功。二次配布は禁止です。cratedbyApartkktrain");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-}
+
 
 
 public function onJoin(PlayerJoinEvent $event)
 {
     $name = $event->getPlayer()->getName();
     $event->setJoinMessage("§a{$name}が参加しました。鯖生活を楽しんでいってね!");
+ }
 }
+
+
+
 
